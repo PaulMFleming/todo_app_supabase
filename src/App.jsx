26287701b6820ea621043c1b817cb7b298fa03f9
Paul Fleming <paul.stillbreathing@gmail.com>
2025-09-14 16:22:48 +0100

    show Todos in App.jsz

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react'
+import { supabase } from './supabase'
+
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    async function fetchTodos() {
+      const { data, error } = await supabase.from('todos').select('*')
+      if (error) {
+        console.error('Error fetching todos:', error)
+      } else {
+        setTodos(data)
+      }
+    }
+    fetchTodos()
+  }, [])
+
   return (
     <div>
       <h1>Todo App</h1>
-      <p>Coming soon...</p>
+      <p>Found {todos.length} todos</p>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
